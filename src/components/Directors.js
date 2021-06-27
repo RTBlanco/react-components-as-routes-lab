e8fb7634b74784570e039333ddd460298ccb448a
RTBlanco <ronnytoribio1@hotmail.com>
2021-06-27 19:29:53 -0400

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -4,7 +4,18 @@ import { directors } from '../data';
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {directors.map((dir, index) => {
+        return(
+          <div key={index}> 
+            <h3>Name: {dir.name}</h3>
+            <p>Genres:</p>
+            <ul>
+              {dir.movies.map((movie, i) => <li key={i}>{movie}</li>)}
+            </ul>
+          </div>
+        )
+      }) }
     </div>
   );
 }
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import { movies } from '../data';
 
+const renderMovies = (movie, idx) => {
+  return (
+    <div key={idx}>
+      <h3>Name: {movie.title}</h3>
+      <p>Time: {movie.time}</p>
+      Genres:
+      <ul>
+        {movie.genres.map((genre, i) => <li key={i}>{genre}</li>)}
+      </ul>
+    </div>
+  )
+}
+
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {movies.map((movie, idx) => renderMovies(movie, idx))}
     </div>
   );
 };
